92f65f202971af0004de4eac5b33642848461f24
Borislav Stanimirov <b.stanimirov@abv.bg>
2020-11-10 15:06:41 +0200

    Parsing CmdSet

--- lib/envy/cmd_set.rb
+++ lib/envy/cmd_set.rb
@@ -6,20 +6,45 @@ module Envy
     end
 
     def self.register_cli_parser(parser)
-      parser.add_cmd(Name, ->(cmd, args) { parse_cmd_line(args) })
+      parser.add_cmd(Name, ->(cmd, args) { parse_cli(args) })
     end
 
     def self.register_script_parser(parser)
       parser.add_cmd(Name, ->(cmd, args) { parse_script(args) })
     end
 
-    def self.parse_cmd_line(args)
-      puts "#{Name} parse_cmd_line"
+    def self.parse_cli(args)
+      opts = CliParser.filter_opts(args)
+
+      # find first instance of equals
+      i = args.find_index { |arg| arg =~ /=/ }
+      raise Envy::Error.new "set: missing '='. Use 'set <name> = <val>'" if !i
+
+      elem = args[i]
+      eq_i = elem.index('=')
+
+      first = elem[0...eq_i]
+      second = elem[eq_i+1..]
+      split = [first, '=', second].select { |s| !s.empty? }
+
+      args[i..i] = split
+
+      i = args.index('=')
+      raise Envy::Error.new "set: bad name '#{args[0...i].join(' ')}'. Use 'set <name> = <val>'" if i != 1
+
+      ParsedCmd.new(CmdSet.new(args[0], args[2..]), opts)
     end
 
     def self.parse_script(args)
       puts "#{Name} parse_script"
     end
+
+    def initialize(name, value)
+      @name = name
+      @value = value
+    end
+
+    attr_accessor :name, :value
 
     def execute(ctx)
       ename = ctx.expand_name(@name)
---
+++ test/test_cmd_set.rb
@@ -0,0 +1,87 @@
+require_relative '../lib/envy'
+require_relative 'helper_opts'
+require_relative 'helper_ctx'
+require 'test/unit'
+
+include Envy
+
+class TestCmdSet < Test::Unit::TestCase
+  def test_cli_parse
+    parsed = CmdSet.parse_cli ['--x', 'foo', '=', 'bar', 'b az', '--y']
+
+    assert_equal parsed.opts, ['--x', '--y']
+    assert_equal parsed.cmd.class, CmdSet
+    assert_equal parsed.cmd.name, 'foo'
+    assert_equal parsed.cmd.value, ['bar', 'b az']
+
+    parsed = CmdSet.parse_cli ['--x', 'foo=', 'bar']
+
+    assert_equal parsed.opts, ['--x']
+    assert_equal parsed.cmd.class, CmdSet
+    assert_equal parsed.cmd.name, 'foo'
+    assert_equal parsed.cmd.value, ['bar']
+
+    parsed = CmdSet.parse_cli ['foo=bar', '--x']
+
+    assert_equal parsed.opts, ['--x']
+    assert_equal parsed.cmd.class, CmdSet
+    assert_equal parsed.cmd.name, 'foo'
+    assert_equal parsed.cmd.value, ['bar']
+
+    parsed = CmdSet.parse_cli ['foo', '=bar']
+
+    assert_equal parsed.opts, []
+    assert_equal parsed.cmd.class, CmdSet
+    assert_equal parsed.cmd.name, 'foo'
+    assert_equal parsed.cmd.value, ['bar']
+
+    parsed = CmdSet.parse_cli ['foo', '=']
+
+    assert_equal parsed.opts, []
+    assert_equal parsed.cmd.class, CmdSet
+    assert_equal parsed.cmd.name, 'foo'
+    assert_equal parsed.cmd.value, []
+
+    parsed = CmdSet.parse_cli ['foo=']
+
+    assert_equal parsed.opts, []
+    assert_equal parsed.cmd.class, CmdSet
+    assert_equal parsed.cmd.name, 'foo'
+    assert_equal parsed.cmd.value, []
+
+    assert_raise(Envy::Error.new "set: missing '='. Use 'set <name> = <val>'") do
+      CmdSet.parse_cli []
+    end
+
+    assert_raise(Envy::Error.new "set: missing '='. Use 'set <name> = <val>'") do
+      CmdSet.parse_cli ['a', 'b', 'c', '--foo']
+    end
+
+    assert_raise(Envy::Error.new "set: bad name ''. Use 'set <name> = <val>'") do
+      CmdSet.parse_cli ['=']
+    end
+
+    assert_raise(Envy::Error.new "set: bad name ''. Use 'set <name> = <val>'") do
+      CmdSet.parse_cli ['=b']
+    end
+
+    assert_raise(Envy::Error.new "set: bad name 'a b'. Use 'set <name> = <val>'") do
+      CmdSet.parse_cli ['a', 'b', '=']
+    end
+  end
+
+  def test_cli_parser
+    parser = CliParser.new(HelperOpts)
+    CmdSet.register_cli_parser(parser)
+    parsed = parser.parse(['--foo', 'set', '--bar', 'foo', '=', 'bar', '-z'])
+    assert_equal parsed.opts, {foo: true}
+    assert_equal parsed.cmds.size, 1
+    assert_equal parsed.cmds[0].cmd.class, CmdSet
+    assert_equal parsed.cmds[0].cmd.name, 'foo'
+    assert_equal parsed.cmds[0].cmd.value, ['bar']
+    assert_equal parsed.cmds[0].opts, {bar: true, baz: true}
+  end
+
+  # def test_execute
+  # end
+end
