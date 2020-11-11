322e8c1c1fc76c65709bf3909356641a1c1faafd
Borislav Stanimirov <b.stanimirov@abv.bg>
2020-11-11 15:25:46 +0200

    Test path vals from TestValBuilder

--- test/test_val_builder.rb
+++ test/test_val_builder.rb
@@ -18,7 +18,7 @@ class TestValBuilder < Test::Unit::TestCase
     assert_equal sv.value, 'foo'
   end
 
-  def test_list_u
+  def test_list_b
     lv = ValBuilder.from_env_string('bar:baz:boz', nil, B)
     assert_instance_of ListVal, lv
     assert_equal lv.ar, ['bar', 'baz', 'boz']
@@ -28,5 +28,29 @@ class TestValBuilder < Test::Unit::TestCase
     lv = ValBuilder.from_env_string('bar;baz;boz', nil, W)
     assert_instance_of ListVal, lv
     assert_equal lv.ar, ['bar', 'baz', 'boz']
+  end
+
+  def test_path_b
+    pv = ValBuilder.from_env_string('/usr/lib/foo', nil, B)
+    assert_instance_of PathVal, pv
+    assert_equal pv.path, '/usr/lib/foo'
+  end
+
+  def test_path_w
+    pv = ValBuilder.from_env_string("D:\\program files\\zzz", nil, W)
+    assert_instance_of PathVal, pv
+    assert_equal pv.path, "D:\\program files\\zzz"
+  end
+
+  def test_path_list_b
+    plv = ValBuilder.from_env_string('/usr/lib/foo:/home/john', nil, B)
+    assert_instance_of PathListVal, plv
+    assert_equal plv.ar, ['/usr/lib/foo', '/home/john']
+  end
+
+  def test_path_list_w
+    plv = ValBuilder.from_env_string("c:\\temp\\xx;d:\\yyy", nil, W)
+    assert_instance_of PathListVal, plv
+    assert_equal plv.ar, ["c:\\temp\\xx", "d:\\yyy"]
   end
 end
