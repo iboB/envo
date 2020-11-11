f1591c1669874367e7e65202fd0cfd5be7bf5829
Borislav Stanimirov <b.stanimirov@abv.bg>
2020-11-11 11:06:35 +0200

    Logger concept

---
+++ lib/envy/logger.rb
@@ -0,0 +1,21 @@
+module Envy
+  class Logger
+    ERROR = 0
+    WARN = 1
+    INFO = 2
+    DEBUG = 3
+    def initialize(max_level)
+      @max_level = max_level
+    end
+    attr_accessor :max_level
+    def log(level, text)
+      return if level > max_level
+      stream = level == 0 ? STDERR : STDOUT
+      stream.puts(text)
+    end
+    def error(text); log(ERROR, text); end
+    def warn(text);  log(WARN,  text); end
+    def puts(text);  log(INFO,  text); end
+    def debug(text); log(DEBUG, text); end
+  end
+end
